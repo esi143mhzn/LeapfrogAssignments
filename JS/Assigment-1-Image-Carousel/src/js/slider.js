a2c16434158f4c718ce8e4c772e8e80c4c22f34c
Pramish Maharjan <pramish143mhzn@gmail.com>
2021-12-26 17:29:29 +0545

    JS/assignment-1-image-carousel is completed

--- JS/Assigment-1-Image-Carousel/src/js/slider.js
+++ JS/Assigment-1-Image-Carousel/src/js/slider.js
@@ -1,91 +1,87 @@
-const wrapper = document.getElementById("wrapper");
-const images = document.querySelector("carousel-image-wrapper");
+const images = document.querySelector(".carousel-image-wrapper");
 
-const imageWidth = 200;
+const imageWidth = 600;
 const imageCount = images.children.length;
 
 images.style.width = `${imageCount * imageWidth}px`;
 
 for (let i = 0; i < imageCount; i++) {
-  const image = images.children[i];
+    const image = images.children[i];
 
-  image.style.left = `${i * imageWidth}px`;
+    image.style.left = `${i * imageWidth}px`;
+}
+
+var dots = document.querySelector('.dots');
+
+for (let i = 1; i < imageCount - 1; i++) {
+    const dot = document.createElement("li");
+    dot.classList.add("dot");
+    dots.appendChild(dot);
+    dot.addEventListener("click", dotClick.bind(null, i), false);
+}
+
+const allDots = dots.querySelectorAll(".dot");
+allDots[0].classList.add("active");
+
+function setActive(index){
+    for(let i = 0; i < imageCount-2 ; i++) {
+        allDots[i].classList.remove("active");
+    }
+    if(index >= imageCount-2){
+        index = 0;
+    }
+    allDots[index].classList.add("active");
+}
+
+function dotClick(index) {
+    currentIndex = index;
+    dx = currentIndex * imageWidth;
+    images.style.left = `-${dx}px`;
+    setActive(index-1);
 }
 
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
-let currentIndex = 0;
+images.style.left = `-600px`;
+let currentIndex = 1;
 
 let interval;
 let dx = 0;
+
 nextBtn.onclick = function () {
-  // const currentImage = images.children[currentIndex];
-  // currentIndex++;
-  // currentImage.style.left = `-${currentIndex * imageWidth}px`;
+    if(currentIndex >= (imageCount - 1)) {
+        currentIndex = 1;
+    }
 
-  currentIndex++;
-  // images.style.left = `-${currentIndex * imageWidth}px`;
+    dx = currentIndex * imageWidth;
 
-  interval = setInterval(() => {
-    dx++;
-    images.style.left = `-${dx}px`;
+    interval = setInterval(() => {
+        dx++;
+        images.style.left = `-${dx}px`;
+        
+        if (dx >= currentIndex * imageWidth) clearInterval(interval);
+    }, 10);
 
-    if (dx >= 200) clearInterval(interval);
-  }, 10);
+    currentIndex++;
+    setActive(currentIndex-1);
 };
 
 prevBtn.onclick = function () {
-  // const currentImage = images.children[currentIndex];
-  // currentIndex++;
-  // currentImage.style.left = `-${currentIndex * imageWidth}px`;
+    if(currentIndex <= 1) {
+        currentIndex = imageCount - 1;
+    }
 
-  currentIndex--;
-  images.style.left = `-${currentIndex * imageWidth}px`;
+    dx = currentIndex * imageWidth;
+    
+    interval = setInterval(() => {
+        dx--;
+        images.style.left = `-${dx}px`;
+        
+        if (dx <= currentIndex * imageWidth) clearInterval(interval);
+    }, 10);
+
+    currentIndex--;
+    setActive(currentIndex-1)
 };
 
-function slider(properties) {
-  // const { imageWidth, imageHeight } = properties;
-
-  properties.imageWidth;
-}
-
-slider({
-  imageWidth: 200,
-  imageHeight: 75
-});
-
-function abd() {
-  // asfas
-  // as
-  // gas
-  // gas
-  // g
-  // asg
-
-  return false;
-}
-
-/**
- *
- * @param {number} width - Width of image
- * @param {number} height - Height of image
- * @param {string} wrapperId - Id of wrapper element
- */
-function Slider(width, height, wrapperId, transitionTime, holdTime) {}
-
-// Magic Numbers
-const inlineImageGap = 4;
-images.style.width = `${
-  imageCount * imageWidth + imageCount * inlineImageGap
-}px`;
-
-// const PI = 3.14159265;
-
-// // const value = PI * r * r;
-
-// const GRAVITY = 9.81;
-// const result = GRAVITY * x + y;
-
-// Math.random();
-// console.log(Math.random() * 5);
